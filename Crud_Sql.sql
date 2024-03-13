49f242e4c3d4a9f39fce961232632c03b7385649
Newder Espinosa <89369940+Goldnew1317@users.noreply.github.com>
2024-03-13 00:24:38 -0400

    Consulta SQL

---
+++ Crud_Sql.sql
@@ -0,0 +1,26 @@
+create database Crud1
+go
+use Crud1
+go
+create table Datos
+(
+	id int identity(1,1) primary key,
+	cedula bigint unique not null,
+	nombre varchar(100),
+	apellido varchar(100),
+	edad int,
+)
+
+
+insert into Datos values
+('12345678901','Sergio','Alcantara','46'),
+('12341512341','Roberto Carlo','Fernandez','16')
+
+select * from Datos
+
+insert into Datos values
+('40230321892','adga','sasdjj','12')
+
+delete from Datos where id=6
+
+update Datos SET nombre = 'angel' where id=8
